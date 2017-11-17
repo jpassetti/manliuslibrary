db2d2826c8c2f74477c1be05d354be24dd6cde3b
Michelle Roth <michelleroth14@gmail.com>
2017-11-17 16:30:40 -0500

    articles images added

---
+++ home-articles.php
@@ -0,0 +1,16 @@
+<div class="images__collection" id="images__article">
+  <ul class="images__list">
+    <li class="images__articles image__one--click">
+      <a class="link__one" href="#">image here</a>
+    </li>
+    <li class="images__articles image__two--click">
+      <a class="link__two" href="#">image here</a>
+    </li>
+    <li class="images__articles image__three--click">
+      <a class="link__three" href="#">image here</a>
+    </li>
+    <li class="images__articles image__four--click">
+      <a class="link__four" href="#">image here</a>
+    </li>
+</div>
+   
